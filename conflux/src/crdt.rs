2b73753ac1481ffcf3d355f8037fc266a04b731e
Kayleexx <sinhamitali56@gmail.com>
2025-10-29 19:29:18 +0530

    feat(crdt): created a crdt state engine along with Yjs compatibility doc

---
+++ conflux/src/crdt.rs
@@ -0,0 +1,49 @@
+use std::sync::{Arc, RwLock};
+use yrs::{
+    Doc, StateVector, Transact, Update, ReadTxn,
+    updates::{decoder::Decode, encoder::Encode},
+};
+
+#[derive(Clone)]
+pub struct CrdtEngine {
+    doc: Arc<RwLock<Doc>>,
+}
+
+impl CrdtEngine {
+    pub fn new() -> Self {
+        Self {
+            doc: Arc::new(RwLock::new(Doc::new())),
+        }
+    }
+
+    pub fn apply_update(&self, update: &[u8]) {
+        let doc = self.doc.read().unwrap();
+        let mut txn = doc.transact_mut();
+        if let Ok(update) = Update::decode_v1(update) {
+            txn.apply_update(update);
+        }
+    }
+
+    pub fn state_vector(&self) -> Vec<u8> {
+        let doc = self.doc.read().unwrap();
+        let txn = doc.transact();
+        txn.state_vector().encode_v1()
+    }
+
+    pub fn encode_state_as_update(&self) -> Vec<u8> {
+        let doc = self.doc.read().unwrap();
+        let txn = doc.transact();
+        txn.encode_state_as_update_v1(&txn.state_vector())
+    }
+
+    pub fn encode_diff(&self, remote_sv: &[u8]) -> Vec<u8> {
+        let doc = self.doc.read().unwrap();
+        let txn = doc.transact();
+        if let Ok(remote) = StateVector::decode_v1(remote_sv) {
+            txn.encode_state_as_update_v1(&remote)
+        } else {
+            vec![]
+        }
+    }
+}
+
